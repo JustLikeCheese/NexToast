ff04cef3b2e74afb469a5547217ab49425dd542e
JustLikeCheese <854675824@qq.com>
2025-11-11 11:36:56 +0800

    test: update test code

--- app/src/main/java/io/github/justlikecheese/nextoast/app/MainActivity.java
+++ app/src/main/java/io/github/justlikecheese/nextoast/app/MainActivity.java
@@ -1,17 +1,21 @@
 package io.github.justlikecheese.nextoast.app;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import io.github.justlikecheese.nextoast.app.R;
 import io.github.justlikecheese.nextoast.NexToast;
 
 public class MainActivity extends Activity {
@@ -19,40 +23,67 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn1 = findViewById(R.id.btn1);
-        Button btn2 = findViewById(R.id.btn2);
-        Button btn3 = findViewById(R.id.btn3);
-        Button btn4 = findViewById(R.id.btn4);
+        Button nextoast1 = findViewById(R.id.nextoast1);
+        Button nextoast2 = findViewById(R.id.nextoast2);
+        Button nextoast3_custom_text_color = findViewById(R.id.nextoast3_custom_text_color);
+        Button nextoast4_custom_layout = findViewById(R.id.nextoast4_custom_layout);
+        Button nextoast5_custom_text = findViewById(R.id.nextoast5_custom_text);
+        Button toast1 = findViewById(R.id.toast1);
+        Button toast2 = findViewById(R.id.toast2);
         final String string1 = "Talk is cheap. Show me the code.\n-- Linus Torvalds";
         final String string2 = "Hello, World!\n你好, 世界!\nहैलो वर्ल्ड!\nمرحبا بالعالم!\nこんにちは世界！\n안녕하세요, 세상아!\nПривет, мир!\nOlá, mundo!";
-        btn1.setOnClickListener(new View.OnClickListener() {
+        nextoast1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 NexToast.makeText(MainActivity.this, string1, NexToast.LENGTH_SHORT).show();
             }
         });
-        btn1.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                NexToast toast = new NexToast(MainActivity.this);
-                toast.setText("YOOOOOOOO~ YOU FOUND ME :)");
-                toast.show();
-                return true;
-            }
-        });
-        btn2.setOnClickListener(new View.OnClickListener() {
+        nextoast2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 NexToast.makeText(MainActivity.this, string2, NexToast.LENGTH_SHORT).show();
             }
         });
-        btn3.setOnClickListener(new View.OnClickListener() {
+        nextoast2.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                NexToast.makeText(MainActivity.this, "YOOOOOOOO~ YOU FOUND ME :)", NexToast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        nextoast3_custom_text_color.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NexToast.makeText(MainActivity.this, toColorfulSpannable("Welcome to use NexToast!"), NexToast.LENGTH_SHORT).show();
+            }
+        });
+        nextoast4_custom_layout.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                NexToast toast = new NexToast(MainActivity.this);
+                Button btn = new Button(MainActivity.this);
+                toast.setView(btn);
+                toast.show();
+            }
+        });
+        nextoast5_custom_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NexToast toast = new NexToast(MainActivity.this);
+                toast.setView();
+                toast.setText(toColorfulSpannable("Welcome to use NexToast!"));
+                toast.getTextView().setTextSize(64);
+                toast.show();
+            }
+        });
+        toast1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, string1, NexToast.LENGTH_SHORT).show();
             }
         });
-        btn4.setOnClickListener(new View.OnClickListener() {
+        toast2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, string2, NexToast.LENGTH_SHORT).show();
@@ -83,5 +114,24 @@ public class MainActivity extends Activity {
             }
         }).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public SpannableString toColorfulSpannable(String text) {
+        SpannableString spannable = new SpannableString(text);
+        int[] rainbowColors = {
+                Color.RED,
+                Color.rgb(255, 165, 0),
+                Color.YELLOW,
+                Color.GREEN,
+                Color.BLUE,
+                Color.rgb(75, 0, 130),
+                Color.MAGENTA
+        };
+        for (int i = 0; i < text.length(); i++) {
+            int colorIndex = i % rainbowColors.length;
+            ForegroundColorSpan span = new ForegroundColorSpan(rainbowColors[colorIndex]);
+            spannable.setSpan(span, i, i + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        return spannable;
     }
 }
